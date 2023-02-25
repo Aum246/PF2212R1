6938e5baf2b2f034bb058df13b184de91487d1a8
Hoai Thu <hthu246@gmail.com>
2023-02-25 21:43:17 +0700

    Loop_15_02

---
+++ Lesson_12-15-02-23/demo_for_loop.js
@@ -0,0 +1,15 @@
+// In ra màn hình các số từ 1 đến 10
+// for (let i = 1; i<11; i++) {
+// document.write(i + `<br>`);
+// }
+// In ra màn hình các số từ 1 đến 10
+// for (let i = 0; i>0; i--) {
+//     document.write(i + `<br>`);
+//     }
+// In ra màn hình các số chẵn từ 1 đến 100
+// for (let i = 1; i<51; i++) {
+//     document.write(i*2 + `<br>`);
+//     }
+for (let i = 0; i<101; i+=2) {
+        document.write(i + `<br>`);
+        }
