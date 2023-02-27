63d77c67cb80e4a6b6a8066da36d1889a9c7d5b7
Hoai Thu <hthu246@gmail.com>
2023-02-27 19:58:36 +0700

    Lesson17_Practice-Array_Bai01

---
+++ Lesson17_27-02-2023/practice-1.js
@@ -0,0 +1,6 @@
+// Bài 1: Hãy viết một chương trình để đảo ngược các phần tử trong list(array)
+let aList = [100, 200, 300, 400, 500];
+
+// bList = [500, 400, 300, 200, 100]
+
+console.log(aList.reverse());
